8241eab5a06fc626e57797d138fc7e32e5114189
Yuehao Qiu <Yuehao.Qiu@synaptics.com>
2019-07-16 19:58:25 +0800

    first commit of python learning

---
+++ search_stock_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.5
+import csv
+#-*-coding:utf-8-*-
+
+def get_stock_num_by_name(name):
+	print("get_stock_num_by_name :", name)
+	with open('./stock/stock.csv', 'r') as f:
+		rander = csv.reader(f)
+		for i in rander:
+			if i[0] == name:
+				return i
+		print("None search")
+		return None
