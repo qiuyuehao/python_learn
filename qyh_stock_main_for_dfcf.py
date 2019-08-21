ea293c1f0d504a3c93e04a5676d594179a38ec68
Yuehao Qiu <Yuehao.Qiu@synaptics.com>
2019-08-21 14:16:38 +0800

    new tag, modify the ui python file and some logic

--- qyh_stock_main_for_dfcf.py
+++ qyh_stock_main_for_dfcf.py
@@ -12,32 +12,60 @@ from send_wechat_msg import send_wechat_msg_to_myself
 import os
 
 
-number_mail_send=[0,0,0,0,0]
-zs_mail_send=[0,0,0,0,0]
+stock_notify_already=[0,0,0,0,0, 0, 0, 0]
+zs_mail_send=[0,0,0,0,0, 0, 0, 0, 0]
 zs_list=['000001', '399001', '399006']
+notify_method = "mail"
+notify_time = 60 * 30
 class MyWidgets(QWidget):
 	msec = 1000
 	pre_date_day = 0;
 	running_cnt = 0
+	stock_notify_already = {}
 	def __init__(self):
 		super().__init__()
 		self.__ui=Ui_MainWindow()
 		self.__ui.setupUi(self)
 		self.__ui.pushButton_2.clicked.connect(self.close)
 		self.__ui.pushButton.clicked.connect(self.start)
-		self.__ui.shangzhen.setText("上证指数")
-		self.__ui.lineEdit_11.setText("1.5")
-		self.__ui.shenzhen.setText("深证成指")
-		self.__ui.lineEdit_12.setText("1.5")
-		self.__ui.chuanyeban.setText("创业板")
-		self.__ui.lineEdit_13.setText("2.0")
+	def get_zs_name(self, zs_str):
+		if zs_str == "000001":
+			return str("上证指数")
+		elif zs_str == "399001":
+			return str("深证指数")
+		elif zs_str == "399006":
+			return str("创业板")
+		else:
+			return str("未知指数")
 	def close(self):
 		os.system('killall -9 qyh_stock_main_for_dfcf.py')
+	def update_notify_value(self, name):
+		print("call update_notify_value name", name)
+		self.stock_notify_already[name] = 0
+	def notify_user_message(self, name, up_down_value, limit, method):
+		if self.stock_notify_already[name] == 0:
+			print("not notify yet")
+			new_timer_thread = threading.Timer(notify_time, self.update_notify_value, (name,)).start()
+			self.stock_notify_already[name] = 1
+		else:
+			#self.stock_notify_already[name] = self.stock_notify_already[name] - 1
+			#print("already notify, just print, count:", self.stock_notify_already[name])
+			method = None
+		if up_down_value > 0:
+			tmp_content = name + "good news  +++  " + str(up_down_value)
+		else:
+			tmp_content = name + "bad news  --- " + str(up_down_value)
+		if method == "wechat":
+			send_wechat_msg_to_myself(tmp_content)
+		elif method == "mail":
+			send_mail_to_myself(tmp_content, "no content")
+		else:
+			print(tmp_content)
 	def start_get_zs_info(self):
 		#print(len(zs_list))
 		cnt = 0
+		zs_info_list = []
 		for str_zs in zs_list:
-			#print(str_zs)
 			return_value = get_gupiao_info(str_zs)
 			if return_value != None:
 				(prePrice, price, time) = return_value
@@ -50,58 +78,43 @@ class MyWidgets(QWidget):
 
 				up_down_value = str(price - prePrice)
 				value = str(price)
-
+				print("bbb")
 				up_down_percent_result = up_down_percent + '%'
-				if cnt == 0:
-					self.__ui.label_4.setText(value)
-					self.__ui.label_5.setText(up_down_percent_result)
-					#self.__ui.label.setText(value)
-					limit = self.__ui.lineEdit_11.text()
-					mail_title = "上证指数 "
-				elif cnt == 1:
-					self.__ui.label_6.setText(value)
-					self.__ui.label_7.setText(up_down_percent_result)
-					limit = self.__ui.lineEdit_12.text()
-					mail_title = "深证成指 "
-				elif cnt == 2:
-					self.__ui.label_8.setText(value)
-					self.__ui.label_9.setText(up_down_percent_result)
-					limit = self.__ui.lineEdit_13.text()
-					mail_title = "创业板 "
-
-				cnt = cnt + 1
+				zs_stock_info = {}
+				
+				zs_stock_info["stock_name"] = self.get_zs_name(str_zs)
+				zs_stock_info["value"] = value
+				zs_stock_info["up_down_percent"] = up_down_percent_result
+				zs_info_list.append(zs_stock_info)
+				if zs_stock_info["stock_name"] in self.stock_notify_already.keys():
+					pass
+				else:
+					self.stock_notify_already[zs_stock_info["stock_name"]] = 0
+				print("aaa")
 				float_up_down = float(up_down_percent)
+				notify = str(self.__ui.get_zs_notify_value(cnt))
+				limit = str(self.__ui.get_zs_limit_value(cnt))
+				cnt = cnt + 1
 				try:
+					notify = float(notify)
 					limit = float(limit)
 				except:
 					continue
-				if abs(float_up_down) > abs(limit):
-					#print("cnt is:", zs_mail_send[cnt - 1])
-					if float_up_down > 0:
-						if (zs_mail_send[cnt - 1] == 0):
-							zs_mail_send[cnt - 1] = 1
-							#print("cnt is:", zs_mail_send[cnt - 1])
-							#send_wechat_msg_to_myself(mail_title+"up up up..." + up_down_percent_result, "congraduation")
-							send_wechat_msg_to_myself(mail_title+"up up up..." + up_down_percent_result + " congraduation")
-						else:
-							#print("cnt is:", zs_mail_send[cnt - 1])
-							print(mail_title+"up up up..." + up_down_percent_result, "congraduation")
-					else:
-						if (zs_mail_send[cnt - 1] == 0):
-							zs_mail_send[cnt - 1] = 1
-							#print("cnt is:", zs_mail_send[cnt - 1])
-							send_wechat_msg_to_myself(mail_title+"down down down..." + up_down_percent_result + " please don't be upset, you don't mean it")
-						else:
-							#print("cnt is:", zs_mail_send[cnt - 1])
-							print(mail_title+"down down down..." + up_down_percent_result, "please don't be upset, you don't mean it")
+				if (abs(float_up_down) > abs(limit)) and (notify == 1):
+					self.notify_user_message(zs_stock_info["stock_name"], float_up_down, limit, notify_method)
+		self.__ui.update_zs_stock_info(zs_info_list)
 	def start_get_info(self):
+		stock_list = []
 		with open("stock_pool.txt", "r") as f:
+			stock_count = 0
 			for number, line in enumerate(f,start=1):
 				line = line.strip('\n')
-				#print(line)
+				if line in self.stock_notify_already.keys():
+					pass
+				else:
+					self.stock_notify_already[line] = 0
 				stock_num=get_stock_num_by_name(line)
 				if stock_num != None:
-					#print("get stock_num:", stock_num[1])
 					return_value = get_gupiao_info(stock_num[1])
 					if return_value != None:
 						(prePrice, price, time) = return_value
@@ -116,94 +129,28 @@ class MyWidgets(QWidget):
 							up_down_percent = str((price -prePrice) / prePrice * 100 - 0.005)[0:5] + '%'
 							up_down_value = str(price - prePrice - 0.005)[0:5]
 						value = str(price)
-						if number == 1:
-							self.__ui.label_10.setText(line)
-							self.__ui.label_12.setText(up_down_percent)
-							self.__ui.label_11.setText(up_down_value)
-							self.__ui.label.setText(value)
-							should_notify = self.__ui.lineEdit.text()
-							limit = self.__ui.lineEdit_2.text()
-							try:
-								limit = float(limit)
-							except:
-								continue
-						if number == 2:
-							self.__ui.label_13.setText(line)
-							self.__ui.label_15.setText(up_down_percent)
-							self.__ui.label_14.setText(up_down_value)
-							self.__ui.label_2.setText(value)
-							should_notify = self.__ui.lineEdit_3.text()
-							limit = self.__ui.lineEdit_4.text()
-							try:
-								limit = float(limit)
-							except:
-								continue
-						if number == 3:
-							self.__ui.label_16.setText(line)
-							self.__ui.label_18.setText(up_down_percent)
-							self.__ui.label_17.setText(up_down_value)
-							self.__ui.label_3.setText(value)
-							should_notify = self.__ui.lineEdit_5.text()
-							limit = self.__ui.lineEdit_6.text()
-							try:
-								limit = float(limit)
-							except:
-								continue
-						if number == 4:
-							self.__ui.label_19.setText(line)
-							self.__ui.label_21.setText(up_down_percent)
-							self.__ui.label_20.setText(up_down_value)
-							self.__ui.label_25.setText(value)
-							should_notify = self.__ui.lineEdit_7.text()
-							limit = self.__ui.lineEdit_8.text()
-							try:
-								limit = float(limit)
-							except:
-								continue
-						if number == 5:
-							self.__ui.label_22.setText(line)
-							self.__ui.label_24.setText(up_down_percent)
-							self.__ui.label_23.setText(up_down_value)
-							self.__ui.label_26.setText(value)
-							should_notify = self.__ui.lineEdit_9.text()
-							limit = self.__ui.lineEdit_10.text()
-							try:
-								limit = float(limit)
-							except:
-								continue
+						stock_list_dict = {}
+						stock_list_dict["stock_name"] = line
+						stock_list_dict["up_down_percent"] = up_down_percent
+						stock_list_dict["up_down_value"] = up_down_value
+						stock_list_dict["value"] = value
+						stock_list.append(stock_list_dict)
+						print(line)
+						float_up_down = float(up_down_percent_num)
+						notify = self.__ui.get_notify_value(stock_count)
+						limit = self.__ui.get_limit_value(stock_count)
 
-						if should_notify == "1":
-							float_up_down = float(up_down_percent_num)
-							if abs(float_up_down) > abs(limit):
-								if float_up_down > 0:
-									if (number_mail_send[number - 1] == 0):
-										number_mail_send[number - 1] = 1
-										send_wechat_msg_to_myself(line+"up up up" + " congraduation")
-									else:
-										print(line+"up up up...", "congraduation")
-								else:
-									if (number_mail_send[number - 1] == 0):
-										number_mail_send[number - 1] = 1
-										send_wechat_msg_to_myself(line+"down down down" + " please don't be upset, you don't mean it")
-									else:
-										print(line+"down down down...", "please don't be upset, you don't mean it")
-	def get_stock_info(self,num_retries=2):
-		#stock_no = self.__ui.lineEdit.text()
-		stock_no = '000002'
-		try:
-			url = 'http://nufm.dfcfw.com/EM_Finance2014NumericApplication/JS.aspx?type=CT&cmd='+stock_no.strip()+'2&sty=CTBF&st=z&sr=&p=&ps=&cb=var%20pie_data=&js=(x)&token=28758b27a75f62dc3065b81f7facb365&_=1496312544427'
-			headers = {'User-agent':'WSWP'}
-			#request = urllib.Request(url,headers=headers)
-			page = urllib.request.urlopen(url)
-			#print(url)
-			page_content = page.read()
-		except urllib.request.URLError as e:
-			print('download error:',e.reason)
-			page_content = None
-			if num_retries > 0:
-				if hasattr(e,'code' and 500 <= e.code <600): # recursively retry 5xx HTTP errors
-					return get_stock_info(stock_no,num_retries-1)
-		return page_content
+						try:
+							notify = float(notify)
+							limit = float(limit)
+						except:
+							print("exception here")
+							continue
+						print("777")
+						if (abs(float_up_down) > abs(limit)) and (notify == 1):
+								self.notify_user_message(stock_list_dict["stock_name"], float_up_down, limit, notify_method)
+						stock_count = stock_count + 1
+		self.__ui.update_stock_info(stock_list)
 	def _update(self):
 		try:
 			self._set_count()
