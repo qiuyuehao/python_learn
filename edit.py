fed8ccb947719cdfd4cb7fa97bd5960fad5445c4
qyh <scutqyh@163.com>
2019-08-24 11:26:13 +0800

    add a file edit window to edit the config text

--- edit.py
+++ edit.py
@@ -9,8 +9,10 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtCore import *
 
 class EditWindow():
+	current_file_name = None
 	def setupUi(self, MainWindow):
 		MainWindow.setObjectName("MainWindow")
 		MainWindow.resize(663, 600)
@@ -29,9 +31,11 @@ class EditWindow():
 		self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
 		self.pushButton_2.setGeometry(QtCore.QRect(510, 120, 99, 27))
 		self.pushButton_2.setObjectName("pushButton_2")
+		self.pushButton_2.clicked.connect(self.save_file)
 		self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
 		self.pushButton_3.setGeometry(QtCore.QRect(510, 190, 99, 27))
 		self.pushButton_3.setObjectName("pushButton_3")
+		self.pushButton_3.clicked.connect(self.cancle_file)
 		MainWindow.setCentralWidget(self.centralwidget)
 		self.menubar = QtWidgets.QMenuBar(MainWindow)
 		self.menubar.setGeometry(QtCore.QRect(0, 0, 663, 25))
@@ -50,12 +54,35 @@ class EditWindow():
 			print("please select a file")
 			return
 		else:
-			print("file select is ",  fileName[0])
-			self.textEdit.setText
-			
+			print("file select is ",  fileName[0], type(fileName[0]))
+			myFile = QFile(fileName[0])
+			if not myFile.open(QFile.ReadOnly | QFile.Text):
+				print("open file wrong", fileName[0])
+				return
+			self.current_file_name = fileName[0]
+			self.mwindow.setWindowTitle(fileName[0])
+			inf = QTextStream(myFile)
+			file_content = inf.readAll()
+			#print(inf.readAll(), type(inf.readAll()))
+			self.textEdit.setPlainText(file_content)
+			myFile.close()
+	def cancle_file(self):
+		self.current_file_name = None
+		self.mwindow.setWindowTitle("please select a file to open")
+		self.textEdit.clear()
+	def save_file(self):
+		myFile = QFile(self.current_file_name)
+		if not myFile.open(QFile.WriteOnly | QFile.Text):
+			print("open to write file wrong", fileName[0])
+			return
+		file_content = self.textEdit.toPlainText()
+		print(file_content)
+		file_content_bytes= bytes(file_content, encoding='utf-8')
+		myFile.write(file_content_bytes)
+		myFile.close()
 	def retranslateUi(self, MainWindow):
 		_translate = QtCore.QCoreApplication.translate
 		MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-		self.pushButton.setText(_translate("MainWindow", "PushButton"))
-		self.pushButton_2.setText(_translate("MainWindow", "PushButton"))
-		self.pushButton_3.setText(_translate("MainWindow", "PushButton"))
+		self.pushButton.setText(_translate("MainWindow", "open"))
+		self.pushButton_2.setText(_translate("MainWindow", "save"))
+		self.pushButton_3.setText(_translate("MainWindow", "cancle"))
